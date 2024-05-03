624f2cd564674542cdaabfdb739a8dbc746ea2eb
Ali raza <raza.abro@binatedigital.com>
2024-05-03 17:40:35 +0500

    push

--- lib/providers/core_provider.dart
+++ lib/providers/core_provider.dart
@@ -440,19 +440,19 @@ class CoreProvider extends ChangeNotifier {
         _homeRecipes = GetReciepiesList.fromJson(response?.data);
         _changeHomeReciepeSate(States.success);
 
-        // if (response?.data['subscribe'] == 0) {
-        //   subscribed = response?.data['subscribe'];
-        //   AppNavigator.pushAndRemoveUntil(
-        //       context,
-        //       SubscriptionScreen(
-        //         isTrial: false,
-        //         showLogout: true,
-        //       ));
-        // } else {
-        //   subscribed = 1;
-        //   getFamilyPoles(context, type, onPollLoaded: onPollLoaded);
-        // }
-        getFamilyPoles(context, type);
+        if (response?.data['subscribe'] == 0) {
+          subscribed = response?.data['subscribe'];
+          AppNavigator.pushAndRemoveUntil(
+              context,
+              SubscriptionScreen(
+                isTrial: false,
+                showLogout: true,
+              ));
+        } else {
+          subscribed = 1;
+          getFamilyPoles(context, type, onPollLoaded: onPollLoaded);
+        }
+        // getFamilyPoles(context, type);
 
         // controller?.refreshCompleted();
 
