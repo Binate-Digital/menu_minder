b9d060809b78909115e3d8e0bfe9e6c3e7793fcf
Saad Ahmed <91254595+hassam123456@users.noreply.github.com>
2025-08-07 00:50:56 +0500

    flutter sdk updated to 3.29.0

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,8 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'app.dart';
 import 'firebase_options.dart';
 
-//Last update 21/aug/2024 (feedbacks done) !!!!
-//Create Meal Receipe work is undone.
+//! Last update 7 AUG 2025
+//! Flutter SDK 3.29.0 (andoird & ios)
 
 void main() async {
   await dotenv.load(fileName: ".env");
